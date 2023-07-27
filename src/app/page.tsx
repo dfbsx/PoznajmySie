15986d6ae391451d0663225dbf0b4f28660e4430
Dominika <werkamdor@gmail.com>
2023-07-27 16:12:09 +0200

    Triangles at background added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+import { Button, Group } from "@mantine/core";
+import styles from "./page.module.css"
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <main>
+      <Group position="right" spacing="md">
+        <Button variant="subtle" color="dark" radius="xs">
+          Zaloguj się
+        </Button>
+        <Button color="dark" radius="xs">
+          Zarejestruj się
+        </Button>
+      </Group>
+      <h1>Hello, Next.js!</h1>;
+      <div className={styles.left}></div>
+      <div className={styles.right}></div>
+    </main>
+  );
 }
