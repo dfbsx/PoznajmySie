294d6328bd595d42ef878bcb059c4ac1fa944597
Dominika <werkamdor@gmail.com>
2023-07-27 14:45:25 +0200

    Google fonts added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,23 @@
 import { MantineProvider } from "../components/mantine";
+import styles from "./globals.module.css";
+import { Biryani } from "next/font/google";
+
+const biryani = Biryani({
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export interface props {
   children?: React.ReactNode;
 }
 
 export default function RootLayout({ children }: props) {
   return (
-    <html lang="en">
+    <html lang="en" className={biryani.className}>
       <MantineProvider>
-        <body>{children}</body>
+        <body className={styles.body}>{children}</body>
       </MantineProvider>
     </html>
   );
