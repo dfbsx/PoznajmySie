9def500119b787ff0e5ce285c7cbd901afe1a066
Dominika <werkamdor@gmail.com>
2023-07-28 15:26:36 +0200

    Landing view prepared

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, Group } from "@mantine/core";
+import { Button, Group, Flex } from "@mantine/core";
 import styles from "./page.module.css";
 import girlGraphic from "../../public/startpagegraphic.png";
 import Image from "next/image";
@@ -15,6 +15,32 @@ export default function Page() {
           Zarejestruj się
         </Button>
       </Group>
+      <Flex
+      className={styles.siteDescription}
+      gap="xs"
+      align="center"
+      direction="column"
+      wrap="wrap"
+    >
+      <div className={styles.container}>
+        <div className={styles.flip}>
+          <div>
+            <div>Jesteś studentem?</div>
+          </div>
+          <div>
+            <div>Masz ochotę na wyjście?</div>
+          </div>
+          <div>
+            <div>Potrzebujesz pomocy w nauce?</div>
+          </div>
+        </div>
+      </div>
+      <p className={styles.appName}>Poznajmy się!</p>
+      <p className={styles.appDescription}>
+        Aplikacja stworzona przez studentów, dla studentów
+      </p>
+      </Flex>
+
       <Image
         className={styles.girlObject}
         src={girlGraphic}
