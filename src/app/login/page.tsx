3423d754bba667789307afcbb2c687fdb4ffcfd0
Dominika <werkamdor@gmail.com>
2023-07-28 15:40:11 +0200

    next router added

---
+++ src/app/login/page.tsx
@@ -0,0 +1,3 @@
+export default function Login(){
+    return <h1>Strona logowania</h1>
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,42 +3,53 @@ import { Button, Group, Flex } from "@mantine/core";
 import styles from "./page.module.css";
 import girlGraphic from "../../public/startpagegraphic.png";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
   return (
     <main>
       <Group position="right" spacing="md">
-        <Button variant="subtle" color="dark" radius="xs">
+        <Button
+          variant="subtle"
+          color="dark"
+          radius="xs"
+          onClick={() => router.push("/login")}
+        >
           Zaloguj się
         </Button>
-        <Button color="dark" radius="xs">
+        <Button
+          color="dark"
+          radius="xs"
+          onClick={() => router.push("/register")}
+        >
           Zarejestruj się
         </Button>
       </Group>
       <Flex
-      className={styles.siteDescription}
-      gap="xs"
-      align="center"
-      direction="column"
-      wrap="wrap"
-    >
-      <div className={styles.container}>
-        <div className={styles.flip}>
-          <div>
-            <div>Jesteś studentem?</div>
-          </div>
-          <div>
-            <div>Masz ochotę na wyjście?</div>
-          </div>
-          <div>
-            <div>Potrzebujesz pomocy w nauce?</div>
+        className={styles.siteDescription}
+        gap="xs"
+        align="center"
+        direction="column"
+        wrap="wrap"
+      >
+        <div className={styles.container}>
+          <div className={styles.flip}>
+            <div>
+              <div>Jesteś studentem?</div>
+            </div>
+            <div>
+              <div>Masz ochotę na wyjście?</div>
+            </div>
+            <div>
+              <div>Potrzebujesz pomocy w nauce?</div>
+            </div>
           </div>
         </div>
-      </div>
-      <p className={styles.appName}>Poznajmy się!</p>
-      <p className={styles.appDescription}>
-        Aplikacja stworzona przez studentów, dla studentów
-      </p>
+        <p className={styles.appName}>Poznajmy się!</p>
+        <p className={styles.appDescription}>
+          Aplikacja stworzona przez studentów, dla studentów
+        </p>
       </Flex>
 
       <Image
