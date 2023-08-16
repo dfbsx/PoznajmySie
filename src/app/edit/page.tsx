10d10a869b995378fc2813011934ccb2194ccb3d
Dominika <werkamdor@gmail.com>
2023-08-16 15:47:42 +0200

    Edit data works properly

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -1,56 +1,71 @@
 "use client";
-import { Avatar, Button, Flex, Group, Indicator, Text, TextInput } from "@mantine/core";
+import {
+  Avatar,
+  Button,
+  Flex,
+  Group,
+  Indicator,
+  Text,
+  TextInput,
+} from "@mantine/core";
 import styles from "../edit/page.module.css";
 import { IconPencil } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
 import { getUserData } from "@/crud/getUserData";
 import { updateUserData } from "@/crud/updateUserData";
 import { useRouter } from "next/navigation";
-export interface UserData{
-    userName: string,
-    bio:string,
-    city:string,
-    university:string,
-    major:string,
-};
+export interface UserData {
+  userName: string;
+  bio: string;
+  city: string;
+  university: string;
+  major: string;
+}
 
 export default function Edit() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [token, setToken] = useState();
-    const router = useRouter();
-    const [userData, setUserData] = useState<UserData>({
-      userName: "",
-      bio: "",
-      city: "",
-      university: "",
-      major: "",
-    });
-  
-    useEffect(() => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [token, setToken] = useState();
+  const router = useRouter();
+  const [userData, setUserData] = useState<UserData>({
+    userName: "",
+    bio: "",
+    city: "",
+    university: "",
+    major: "",
+  });
+
+  useEffect(() => {
     const user = JSON.parse(localStorage.getItem("PoznajmySie") || "{}");
-      setToken(user.token);
-      getUserData(user.token)
-        .then((resp) => {
-          setUserData(resp.data);
-          setIsLoading(false);
-        })
-        .catch((err) => {
-          setIsLoading(false);
-          alert(err.response.data.title?err.response.data.title:"Wystąpił nieznany błąd")
-        });
-    }, []);
-  
-    const handleUpdateData = () => {
-      updateUserData(userData,token)
-        .then(() => {
-          console.log("jej");
-          console.log(userData);
-        })
-        .catch((error:any) => {
-          console.log( error.response.data.title);
-          alert(error.response.data.title?error.response.data.title:"Wystąpił nieznany błąd")
-        });
-    };
+    setToken(user.token);
+    getUserData(user.token)
+      .then((resp) => {
+        setUserData(resp.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        alert(
+          err.response.data.title
+            ? err.response.data.title
+            : "Wystąpił nieznany błąd"
+        );
+      });
+  }, []);
+
+  const handleUpdateData = () => {
+    updateUserData(userData, token)
+      .then(() => {
+        router.back();
+      })
+      .catch((error: any) => {
+        console.log(error.response.data.title);
+        alert(
+          error.response.data.title
+            ? error.response.data.title
+            : "Wystąpił nieznany błąd"
+        );
+      });
+  };
   return (
     <div className={styles.page}>
       <div className={styles.avatarColumn}>
@@ -58,7 +73,7 @@ export default function Edit() {
           <Indicator
             inline
             label={<IconPencil size={20} strokeWidth={1.5} color={"white"} />}
-            color="blue"
+            color="#68AEFF"
             size={30}
             offset={28}
             radius="xl"
@@ -71,52 +86,65 @@ export default function Edit() {
             />
           </Indicator>
         </Group>
-        <Text fz={36} fw={600}>{userData.userName}</Text>
+        <Text fz={36} fw={600}>
+          {userData.userName}
+        </Text>
       </div>
-      <Flex className={styles.dataColumn}  align="flex-start" justify="space-between" gap="xl">
-      <TextInput
+      <Flex
+        className={styles.dataColumn}
+        align="flex-start"
+        justify="space-between"
+        gap="xl"
+      >
+        <TextInput
           className={styles.input}
-      placeholder={userData.bio}
-      label="Bio"
-      size="md"
-    />
-     <TextInput
-         className={styles.input}
-      placeholder={userData.city}
-      label="Miasto"
-      size="md"
-    />
-     <TextInput
-         className={styles.input}
-      placeholder={userData.university}
-      label="Uczelnia"
-      size="md"
-    />
-     <TextInput
-     className={styles.input}
-      placeholder={userData.major}
-      label="Kierunek"
-      size="md"
-    />
-     <Group position="right" spacing="xl">
-        <Button
-        className={styles.button}
-          variant="subtle"
-          color="dark"
-          radius="xs"
-          onClick={() => router.push("/home")}
-        >
-          Wróć do strony głównej
-        </Button>
-        <Button
-        className={styles.button}
-          color="dark"
-          radius="xs"
-          onClick={() => router.push("/register")}
-        >
-          Zapisz zmiany
-        </Button>
-      </Group>
+          label="Bio"
+          size="md"
+          value={userData.bio}
+          onChange={(e) => setUserData({ ...userData, bio: e.target.value })}
+        />
+        <TextInput
+          className={styles.input}
+          label="Miasto"
+          size="md"
+          value={userData.city}
+          onChange={(e) => setUserData({ ...userData, city: e.target.value })}
+        />
+        <TextInput
+          className={styles.input}
+          label="Uczelnia"
+          size="md"
+          value={userData.university}
+          onChange={(e) =>
+            setUserData({ ...userData, university: e.target.value })
+          }
+        />
+        <TextInput
+          className={styles.input}
+          label="Kierunek"
+          size="md"
+          value={userData.major}
+          onChange={(e) => setUserData({ ...userData, major: e.target.value })}
+        />
+        <Group position="right" spacing="xl">
+          <Button
+            className={styles.button}
+            variant="subtle"
+            color="dark"
+            radius="xs"
+            onClick={() => router.push("/home")}
+          >
+            Wróć do strony głównej
+          </Button>
+          <Button
+            className={styles.button}
+            color="dark"
+            radius="xs"
+            onClick={handleUpdateData}
+          >
+            Zapisz zmiany
+          </Button>
+        </Group>
       </Flex>
     </div>
   );
